ea3fb097cad04098f13fbfda0eec15961e1e8b33
Enot3811 <enot3811@gmail.com>
2023-09-08 15:49:56 +0300

    Get started implementation of cvat dataset

--- cvat_segmentation_dataset.py
+++ cvat_segmentation_dataset.py
@@ -2,6 +2,10 @@ from typing import List, Dict, Union, Any, Tuple
 from pathlib import Path
 import xml.etree.ElementTree as ET
 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 
 class CvatSegmentationDataset:
 
@@ -11,6 +15,7 @@ class CvatSegmentationDataset:
         annots = ET.parse(dset_pth / 'annotations.xml').getroot()
         
         # Get labels
+        self.label_to_color = {}
         self.labels: List[Dict[str, Any]] = []
         labels_annots = annots.findall('meta/job/labels/label')
         for label_annot in labels_annots:
@@ -18,18 +23,18 @@ class CvatSegmentationDataset:
             hex_color = label_annot.find('color').text
             color = (int(hex_color[i:i + 2], 16)
                      for i in range(1, len(hex_color), 2))
-            self.labels.append({
-                'name': name,
-                'color': color})
+            self.label_to_color[name] = color
 
         # Get images
         self.images: List[Dict[str, Any]] = []
         imgs_annots = annots.findall('image')
         for img_annots in imgs_annots:
             name = img_annots.get('name')
+            shape = (int(img_annots.get('height')),
+                     int(img_annots.get('width')))
             polygons = img_annots.findall('polygon')
             labels: List[str] = []
-            pts: List[Tuple[int, int]] = []
+            polygons_pts: List[List[Tuple[int, int]]] = []
             for polygon in polygons:
                 labels.append(polygon.get('label'))
                 pts_str = polygon.get('points')
@@ -38,21 +43,43 @@ class CvatSegmentationDataset:
                     lambda xy: tuple(map(lambda cord: int(float(cord)),
                                          xy.split(','))),
                     pts))
+                polygons_pts.append(pts)
             self.images.append({
                 'name': name,
                 'labels': labels,
-                'points': pts
+                'polygons': polygons_pts,
+                'shape': shape
             })
+        # TODO с цветами что-то не то
 
-    def __getitem__(self) -> Dict[str, Any]:
-        # TODO вернуть из images дикт по индексу
-        pass
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        return self.images[idx]
 
-    def get_labels(self) -> List[str]:
-        # TODO docs
+    def get_labels(self) -> List[Dict[str, Any]]:
+        """Get labels with corresponding colors.
+
+        Returns
+        -------
+        List[Dict[str, Any]]
+            List of dicts that have "name" and "color" keys.
+        """
         return self.labels
     
-    # TODO Написать функцию конвертации полигонов в cv2 картинку
+    def create_segmentation_masks(self):
+        for image_annots in self.images:
+            h, w = image_annots['shape']
+            polygons = image_annots['polygons']
+            labels = image_annots['labels']
+            name = image_annots['name']
+            canvas = np.zeros((h, w, 3), np.uint8)
+            for polygon, label in zip(polygons, labels):
+                cv2.fillPoly(canvas, polygon, self.label_to_color[label])
+                
+            plt.imshow(canvas)
+            plt.show()
+    
+        # TODO Написать функцию конвертации полигонов в cv2 картинку
 
 
 dset = CvatSegmentationDataset('/home/pc0/projects/segmentation/data/test/')
+dset.create_segmentation_masks()
