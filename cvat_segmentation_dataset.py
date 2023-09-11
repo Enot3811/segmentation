4cd7632393ad533ead9bee417af321a4c7be705f
Enot3811 <enot3811@gmail.com>
2023-09-11 18:43:20 +0300

    implement masks creating

--- cvat_segmentation_dataset.py
+++ cvat_segmentation_dataset.py
@@ -4,26 +4,30 @@ import xml.etree.ElementTree as ET
 
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
 
 class CvatSegmentationDataset:
 
     def __init__(self, dset_pth: Union[Path, str]) -> None:
         if isinstance(dset_pth, str):
-            dset_pth = Path(dset_pth)
-        annots = ET.parse(dset_pth / 'annotations.xml').getroot()
+            self.dset_pth = Path(dset_pth)
+        else:
+            self.dset_pth = dset_pth
+        annots = ET.parse(self.dset_pth / 'annotations.xml').getroot()
         
         # Get labels
-        self.label_to_color = {}
-        self.labels: List[Dict[str, Any]] = []
+        self._label_to_color = {}
+        self._labels = []
+        self._label_to_idx = {}
         labels_annots = annots.findall('meta/job/labels/label')
-        for label_annot in labels_annots:
+        for i, label_annot in enumerate(labels_annots):
             name = label_annot.find('name').text
             hex_color = label_annot.find('color').text
-            color = (int(hex_color[i:i + 2], 16)
-                     for i in range(1, len(hex_color), 2))
-            self.label_to_color[name] = color
+            color = [int(hex_color[j:j + 2], 16)
+                     for j in range(1, len(hex_color), 2)]
+            self._label_to_color[name] = color
+            self._labels.append(name)
+            self._label_to_idx[name] = i
 
         # Get images
         self.images: List[Dict[str, Any]] = []
@@ -33,10 +37,12 @@ class CvatSegmentationDataset:
             shape = (int(img_annots.get('height')),
                      int(img_annots.get('width')))
             polygons = img_annots.findall('polygon')
-            labels: List[str] = []
+            labels: List[int] = []
             polygons_pts: List[List[Tuple[int, int]]] = []
             for polygon in polygons:
-                labels.append(polygon.get('label'))
+                label = polygon.get('label')
+                label_idx = self._label_to_idx[label]
+                labels.append(label_idx)
                 pts_str = polygon.get('points')
                 pts = pts_str.split(';')
                 pts = list(map(
@@ -50,22 +56,63 @@ class CvatSegmentationDataset:
                 'polygons': polygons_pts,
                 'shape': shape
             })
-        # TODO с цветами что-то не то
 
     def __getitem__(self, idx: int) -> Dict[str, Any]:
         return self.images[idx]
+    
+    def get_labels(self) -> List[str]:
+        """Get a list of dataset's labels
 
-    def get_labels(self) -> List[Dict[str, Any]]:
+        Returns
+        -------
+        List[str]
+            The list of the labels.
+        """
+        return self._labels
+
+    def get_labels_colors(self) -> Dict[str, Tuple[int, int, int]]:
         """Get labels with corresponding colors.
 
         Returns
         -------
-        List[Dict[str, Any]]
-            List of dicts that have "name" and "color" keys.
+        Dict[str, Tuple[int, int, int]]
+            Dict that contains labels as keys and "color" as values.
         """
-        return self.labels
+        return self._label_to_color
     
-    def create_segmentation_masks(self):
+    def create_segmentation_masks(self, save_path: Path = None):
+        """Create label masks of dataset's annotations.
+
+        Parameters
+        ----------
+        save_path : Path, optional
+            Path to save dir.
+            If not provided then masks will be save in dataset's directory.
+        """
+        if save_path is None:
+            save_path = self.dset_pth / 'masks'
+        for image_annots in self.images:
+            h, w = image_annots['shape']
+            polygons = image_annots['polygons']
+            labels = image_annots['labels']
+            name = image_annots['name']
+            canvas = np.zeros((h, w), np.uint8)
+            for polygon, label in zip(polygons, labels):
+                cv2.fillPoly(canvas, [np.array(polygon)], [label])
+            npy_name = name.split('.')[0] + '.npy'
+            np.save(save_path / npy_name, canvas)
+        
+    def create_color_masks(self, save_path: Path = None):
+        """Create rgb masks of dataset's annotations.
+
+        Parameters
+        ----------
+        save_path : Path, optional
+            Path to save dir.
+            If not provided then masks will be save in dataset's directory.
+        """
+        if save_path is None:
+            save_path = self.dset_pth / 'color_masks'
         for image_annots in self.images:
             h, w = image_annots['shape']
             polygons = image_annots['polygons']
@@ -73,13 +120,11 @@ class CvatSegmentationDataset:
             name = image_annots['name']
             canvas = np.zeros((h, w, 3), np.uint8)
             for polygon, label in zip(polygons, labels):
-                cv2.fillPoly(canvas, polygon, self.label_to_color[label])
-                
-            plt.imshow(canvas)
-            plt.show()
-    
-        # TODO Написать функцию конвертации полигонов в cv2 картинку
+                cv2.fillPoly(canvas, [np.array(polygon)],
+                             self.get_labels_colors()[label])
+            np.save(save_path / name)
 
 
 dset = CvatSegmentationDataset('/home/pc0/projects/segmentation/data/test/')
 dset.create_segmentation_masks()
+dset.create_color_masks()
